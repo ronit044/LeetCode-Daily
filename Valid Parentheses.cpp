ebf3c3d9d601fcc458bfe3c424541be7568c2508
Ronit_sharma004 <118843577+ronit044@users.noreply.github.com>
2023-09-14 16:43:52 +0530

    Create Valid Parentheses.cpp

---
+++ Valid Parentheses.cpp
@@ -0,0 +1,60 @@
+// Question:
+// Valid Parentheses
+// Hint
+// Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+// An input string is valid if:
+// Open brackets must be closed by the same type of brackets.
+// Open brackets must be closed in the correct order.
+// Every close bracket has a corresponding open bracket of the same type.
+
+// Example 1:
+// Input: s = "()"
+// Output: true
+
+// Example 2:
+// Input: s = "()[]{}"
+// Output: true
+
+// Example 3:
+// Input: s = "(]"
+// Output: false
+ 
+// Constraints:
+// 1 <= s.length <= 104
+// s consists of parentheses only '()[]{}'
+
+// solution:
+
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        stack<char> stck;
+
+        if(s[0]=='}' || s[0]==']' || s[0]==')')
+        return false;
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='(' || s[i]=='{' || s[i]=='[')
+            {
+                st.push(s[i]);
+            }
+            else if((s[i]==')' || s[i]=='}' || s[i]==']')){
+                 if (st.empty()) {
+                    return false; 
+                }
+                if(s[i]==')' && st.top()=='(')
+                st.pop();
+                else if(s[i]=='}' && st.top()=='{')
+                st.pop();
+                else if(s[i]==']' && st.top()=='[')
+                st.pop();
+                else
+                return false;
+            }
+        }
+        if(st.empty())
+        return true;
+        return false;
+    }
+};
