09ab7be2cd5e9eeebbc7fafc964b6c5e13270445
Ronit_sharma004 <118843577+ronit044@users.noreply.github.com>
2024-06-22 19:16:53 +0530

    Time: 31 ms (56.19%) | Memory: 50.3 MB (84.18%) - LeetSync

--- 1014-k-closest-points-to-origin/k-closest-points-to-origin.java
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.java
@@ -1,31 +1,15 @@
 class Solution {
     public int[][] kClosest(int[][] points, int k) {
-        int l=points.length;
-        double[] arr=new double[l];
-        for(int i=0;i<l;i++){
-            int ar[]=points[i];
-            double x=Math.sqrt(Math.pow(ar[0],2)+Math.pow(ar[1],2));
-            arr[i]=x;
-        }
-        for(int i=0;i<l;i++){
-            for(int j=0;j<l-i-1;j++)
-            {
-                if(arr[j]>arr[j+1]){
-                    double x=arr[j];
-                    int[] ar=points[j];
-
-                    arr[j]=arr[j+1];
-                    points[j]=points[j+1];
-
-                    arr[j+1]=x;
-                    points[j+1]=ar;
-                }
-            }
-        }
-        int[][] res=new int[k][2];
-        for(int i=0;i<k;i++){
-            res[i]=points[i];
-        }
-        return res;
+       PriorityQueue<int[]> pq=new PriorityQueue<>((p1, p2) -> p2[0] * p2[0] + p2[1] * p2[1] - p1[0] * p1[0] - p1[1] * p1[1]);
+       for(int[] x:points){
+        pq.add(x);
+        if(pq.size()>k) pq.poll();
+       }
+       int res[][]=new int[k][2];
+       while(k>0){
+        res[--k]=pq.poll();
+      
+       }
+       return res;
     }
 }
