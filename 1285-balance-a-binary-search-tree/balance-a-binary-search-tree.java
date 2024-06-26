49d2f5afa5cd474a016c61401f42066703aa80a2
Ronit_sharma004 <118843577+ronit044@users.noreply.github.com>
2024-06-26 09:47:42 +0530

    Time: 2 ms (96.27%) | Memory: 45.8 MB (50.87%) - LeetSync

--- 1285-balance-a-binary-search-tree/balance-a-binary-search-tree.java
+++ 1285-balance-a-binary-search-tree/balance-a-binary-search-tree.java
@@ -19,21 +19,15 @@ class Solution {
             return null;
         }
 
-        // Get the middle element and make it root
         int mid = (start + end) / 2;
         TreeNode node = new TreeNode(nodes.get(mid));
 
-        // Recursively construct the left and right subtrees
         node.left = sortedArrayToBST(nodes, start, mid - 1);
         node.right = sortedArrayToBST(nodes, mid + 1, end);
 
         return node;
     }
 
-    // Function to balance an unbalanced BST
-    TreeNode balanceBST(TreeNode root,ArrayList<Integer> lt) {
-        return sortedArrayToBST(lt, 0, lt.size() - 1);
-    }
     public void inOrder(TreeNode root,ArrayList<Integer> lt){
         if(root==null) return;
         inOrder(root.left,lt);
@@ -43,6 +37,6 @@ class Solution {
     public TreeNode balanceBST(TreeNode root) {
         ArrayList<Integer> lt=new ArrayList<>();
         inOrder(root,lt);
-        return balanceBST(root,lt);
+        return sortedArrayToBST(lt, 0, lt.size() - 1);
     }
 }
