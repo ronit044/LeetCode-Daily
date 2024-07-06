44468af8e0179dd7e522160b3e2ffa1e0b21d72e
Ronit_sharma004 <118843577+ronit044@users.noreply.github.com>
2024-07-06 18:19:56 +0530

    Time: 0 ms (100.00%) | Memory: 40.8 MB (51.13%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.java
+++ 213-house-robber-ii/house-robber-ii.java
@@ -1,26 +1,23 @@
 class Solution {
-    public int helper(int[] nums, int start, int end) {
-        int[] dp = new int[nums.length];
-        Arrays.fill(dp, -1);
-        return robFrom(nums, start, end, dp);
-    }
-    
-    public int robFrom(int[] nums, int ind, int end, int[] dp) {
-        if (ind > end) return 0;
-        if (dp[ind] != -1) return dp[ind];
-        
-        int rob = nums[ind] + robFrom(nums, ind + 2, end, dp);
-        int skip = robFrom(nums, ind + 1, end, dp);
-        dp[ind] = Math.max(rob, skip);
-        
-        return dp[ind];
+    public int helper(int[] nums, int ind,int end, int[] dp){
+       if(ind>end) return 0;
+       if(dp[ind]!=-1) return dp[ind];
+       int a=nums[ind]+helper(nums,ind+2,end,dp);
+       int b=helper(nums,ind+1,end,dp);
+       dp[ind]=Math.max(a,b);
+       return dp[ind];
     }
 
     public int rob(int[] nums) {
         if (nums.length == 1) return nums[0];
         
-        int max1 = helper(nums, 0, nums.length - 2);
-        int max2 = helper(nums, 1, nums.length - 1);
+        int[] dp1 = new int[nums.length];
+        int[] dp2 = new int[nums.length];
+        Arrays.fill(dp1,-1);
+        Arrays.fill(dp2,-1);
+      
+        int max1 = helper(nums, 0, nums.length - 2,dp1);
+        int max2 = helper(nums, 1, nums.length - 1,dp2);
         
         return Math.max(max1, max2);
     }
