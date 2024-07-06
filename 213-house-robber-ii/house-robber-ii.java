f2fc0597fa753ab2ab59d9ed60c28f491026ee85
Ronit_sharma004 <118843577+ronit044@users.noreply.github.com>
2024-07-06 18:13:44 +0530

    Time: 0 ms (100.00%) | Memory: 40.7 MB (81.62%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.java
+++ 213-house-robber-ii/house-robber-ii.java
@@ -1,18 +1,27 @@
 class Solution {
-    public int rob(int[] nums) {
-        if (nums.length == 1) {
-            return nums[0];
-        }
-        return Math.max(robLinear(nums, 0, nums.length - 2), robLinear(nums, 1, nums.length - 1));
+    public int helper(int[] nums, int start, int end) {
+        int[] dp = new int[nums.length];
+        Arrays.fill(dp, -1);
+        return robFrom(nums, start, end, dp);
+    }
+    
+    public int robFrom(int[] nums, int ind, int end, int[] dp) {
+        if (ind > end) return 0;
+        if (dp[ind] != -1) return dp[ind];
+        
+        int rob = nums[ind] + robFrom(nums, ind + 2, end, dp);
+        int skip = robFrom(nums, ind + 1, end, dp);
+        dp[ind] = Math.max(rob, skip);
+        
+        return dp[ind];
     }
 
-    private int robLinear(int[] nums, int start, int end) {
-        int prev2 = 0, prev1 = 0, current = 0;
-        for (int i = start; i <= end; i++) {
-            current = Math.max(prev1, prev2 + nums[i]);
-            prev2 = prev1;
-            prev1 = current;
-        }
-        return current;
+    public int rob(int[] nums) {
+        if (nums.length == 1) return nums[0];
+        
+        int max1 = helper(nums, 0, nums.length - 2);
+        int max2 = helper(nums, 1, nums.length - 1);
+        
+        return Math.max(max1, max2);
     }
 }
