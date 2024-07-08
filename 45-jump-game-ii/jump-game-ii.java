796f3f97533ea421357b0c8f5920687a29398b1c
Ronit_sharma004 <118843577+ronit044@users.noreply.github.com>
2024-07-08 23:35:21 +0530

    Time: 129 ms (7.99%) | Memory: 46 MB (5.47%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.java
+++ 45-jump-game-ii/jump-game-ii.java
@@ -2,11 +2,10 @@ import java.util.Arrays;
 
 class Solution {
     public int helper(int[] nums, int i, int[] dp) {
-        if (i >= nums.length - 1) return 0;  // Reached or surpassed the end
-        if (dp[i] != -1) return dp[i];  // Memoized result
-        
-        int minJumps = Integer.MAX_VALUE - 1; // Set to a large number, minus 1 to avoid overflow
-        for (int j = 1; j <= nums[i]; j++) {  // j starts from 1 to nums[i]
+        if (i >= nums.length - 1) return 0;  
+        if (dp[i] != -1) return dp[i];  
+        int minJumps = Integer.MAX_VALUE - 1;
+        for (int j = 1; j <= nums[i]; j++) {  
             if (i + j < nums.length) {
                 minJumps = Math.min(minJumps, 1 + helper(nums, i + j, dp));
             }
