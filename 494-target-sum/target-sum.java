99df1021b01ba5d6f4257a8b8487a62395c75e1e
Ronit_sharma004 <118843577+ronit044@users.noreply.github.com>
2024-07-09 09:40:59 +0530

    Time: 10 ms (60.47%) | Memory: 44.9 MB (16.98%) - LeetSync

--- 494-target-sum/target-sum.java
+++ 494-target-sum/target-sum.java
@@ -1,20 +1,22 @@
 class Solution {
-    int count=0;
-    public void helper(int[] nums,int ind,int sum,int t,int[] dp){
-        if(ind>nums.length-1){
-            if(sum==t)
-            {
-                count++;
+    public int helper(int[] nums, int ind, int sum, int t, int[][] dp) {
+        if (ind > nums.length - 1) {
+            if (sum == t) {
+                return 1;
             }
-            return;
+            return 0;
         }
-        helper(nums,ind+1,(sum-nums[ind]),t,dp);
-        helper(nums,ind+1,(sum+nums[ind]),t,dp);
+        if (dp[ind][sum + 1000] != -1) return dp[ind][sum + 1000]; // shifting sum by 1000 to handle negative indices
+        int a = helper(nums, ind + 1, sum - nums[ind], t, dp);
+        int b = helper(nums, ind + 1, sum + nums[ind], t, dp);
+        return dp[ind][sum + 1000] = a + b;
     }
+
     public int findTargetSumWays(int[] nums, int target) {
-        int[] dp=new int[nums.length];
-        Arrays.fill(dp,-1);
-        helper(nums,0,0,target,dp);
-        return count;
+        int[][] dp = new int[nums.length][2001]; // range of sum is -1000 to 1000
+        for (int[] row : dp) {
+            Arrays.fill(row, -1);
+        }
+        return helper(nums, 0, 0, target, dp);
     }
 }
