b1cbd35a0d06e69c411d88d76a12adfda7256d16
Ronit_sharma004 <118843577+ronit044@users.noreply.github.com>
2024-07-16 22:37:31 +0530

    Time: 108 ms (40.54%) | Memory: 57.5 MB (34.30%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
@@ -1,45 +1,48 @@
 class Solution {
+    public void sml(int[] heights,int[] res){
+        Stack<Integer> st=new Stack<>();
+        int ind=-1;
+        for(int i=0;i<heights.length;i++){
+            ind=-1;
+            while(!st.isEmpty()){
+                if(heights[st.peek()]<heights[i]){
+                    ind=st.peek();
+                    break;
+                }
+                st.pop();
+            }
+            res[i]=ind;
+            st.add(i);          
+        }
+    }
+    public void smr(int[] heights,int[] res){
+        Stack<Integer> st=new Stack<>();
+        int ind=-1;
+        for(int i=heights.length-1;i>=0;i--){
+            ind=heights.length;
+            while(!st.isEmpty()){
+                if(heights[st.peek()]<heights[i]){
+                    ind=st.peek();
+                    break;
+                }
+                st.pop();
+            }
+            res[i]=ind;
+            st.add(i);          
+        }
+    }
+
     public int largestRectangleArea(int[] heights) {
         int l=heights.length;
-        int sml[][]=new int[l][2];
-        int smr[][]=new int[l][2];
-        int p=0;
-        Stack<int[]> st=new Stack();
+        int smlArr[]=new int[l];
+        int smrArr[]=new int[l];
+        sml(heights,smlArr);
+        smr(heights,smrArr);
+        int res=0;
         for(int i=0;i<l;i++){
-            int x=-1;
-            int ind=-1;
-            while(!st.isEmpty()){
-                if(st.peek()[0]<heights[i]){
-                    x=st.peek()[0];
-                    ind=st.peek()[1];
-                    break;
-                }
-                st.pop();
-            }
-            sml[p++]=new int[]{x,ind};
-            st.push(new int[]{heights[i],i});
+            int x=(smrArr[i]-smlArr[i]-1)*heights[i];
+            res=Math.max(res,x);
         }
-        st.clear();
-        p=l-1;
-        for(int i=l-1;i>=0;i--){
-            int x=l;
-            int ind=l;
-            while(!st.isEmpty()){
-                if(st.peek()[0]<heights[i]){
-                    x=st.peek()[0];
-                    ind=st.peek()[1];
-                    break;
-                }
-                st.pop();
-            }
-            smr[p--]=new int[]{x,ind};
-            st.push(new int[]{heights[i],i});
-        }
-        int ans=Integer.MIN_VALUE;
-        for(int i=0;i<l;i++){
-            int h=(smr[i][1]-sml[i][1]-1)*heights[i];
-            ans=Math.max(ans,h);
-        }
-        return ans;
+        return res;
     }
 }
