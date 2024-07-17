ada28c7216f0f36fda5af47522051331952a4bc1
Ronit_sharma004 <118843577+ronit044@users.noreply.github.com>
2024-07-17 16:15:39 +0530

    Time: 1 ms (100.00%) | Memory: 44.6 MB (23.23%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.java
+++ 2-add-two-numbers/add-two-numbers.java
@@ -10,21 +10,40 @@
  */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode head=new ListNode();
-        ListNode dummy=head;
-        int sum=0,carry=0;
-        while(l1!=null || l2!=null || carry!=0){
-            int d1=l1!=null?l1.val:0;
-            int d2=l2!=null?l2.val:0;
-            sum=d1+d2+carry;
+        ListNode dummy1=l1,dummy2=l2;
+        ListNode dummyHead=null;
+        ListNode dummy=dummyHead;
+        int carry=0;
+        while(dummy1!=null && dummy2!=null){
+            int sum=dummy1.val+dummy2.val+carry;
             carry=sum/10;
-            int num=sum%10;
-            ListNode y=new ListNode(num);
-            dummy.next=y;
-            dummy=dummy.next;
-            l1=l1!=null?l1.next:null;
-            l2=l2!=null?l2.next:null;
+            int finalSum=sum%10;
+            if(dummyHead==null){
+                dummyHead=new ListNode(finalSum);
+                dummy=dummyHead;
+            }
+            else{
+                dummy.next=new ListNode(finalSum);
+                dummy=dummy.next;
+            }
+            dummy1=dummy1.next;
+            dummy2=dummy2.next;
         }
-        return head.next;
+        while(dummy1!=null){
+            int sum=dummy1.val+carry;
+            carry=sum/10;
+            dummy.next=new ListNode(sum%10);
+            dummy=dummy.next;
+            dummy1=dummy1.next;
+        }
+        while(dummy2!=null){
+            int sum=dummy2.val+carry;
+            carry=sum/10;
+            dummy.next=new ListNode(sum%10);
+            dummy=dummy.next;
+            dummy2=dummy2.next;
+        }
+        if(carry!=0) dummy.next=new ListNode(carry);
+        return dummyHead;
     }
 }
