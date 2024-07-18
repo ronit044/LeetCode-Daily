420bd2e7ad5fd22174e5afd9c39a071aedc706bc
Ronit_sharma004 <118843577+ronit044@users.noreply.github.com>
2024-07-18 13:46:22 +0530

    Time: 26 ms (5.16%) | Memory: 56.7 MB (6.51%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,16 +1,16 @@
 class Solution {
-    int res=0;
-    private int robLinear(int[] nums, int start, int end) {
-        int prev2 = 0, prev1 = 0, current = 0;
-        for (int i = start; i <= end; i++) {
-            current = Math.max(prev1, prev2 + nums[i]);
-            prev2 = prev1;
-            prev1 = current;
-        }
-        return current;
+    public int helper(int[] nums,int ind,int sum,int[][] dp){
+        if(ind>nums.length-1) return sum;
+        if(dp[ind][sum]!=-1) return dp[ind][sum];
+        // if(ind==nums.length-1) return sum;
+        int a=helper(nums,ind+2,sum+nums[ind],dp);
+        int b=helper(nums,ind+1,sum,dp);
+        return dp[ind][sum]=Math.max(a,b);
     }
     public int rob(int[] nums) {
-        return robLinear(nums,0,nums.length-1);
-        // return res;
+        int[][] dp=new int[nums.length][10001];
+        for(int i=0;i<nums.length;i++)
+        Arrays.fill(dp[i],-1);
+        return helper(nums,0,0,dp);
     }
 }
