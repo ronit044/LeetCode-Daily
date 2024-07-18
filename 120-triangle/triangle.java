c856bb363d459e9861afe1f19fe98f62e10664f3
Ronit_sharma004 <118843577+ronit044@users.noreply.github.com>
2024-07-18 13:28:06 +0530

    Time: 1 ms (99.51%) | Memory: 44 MB (60.44%) - LeetSync

--- 120-triangle/triangle.java
+++ 120-triangle/triangle.java
@@ -1,18 +1,19 @@
 class Solution {
-    public int helper(List<List<Integer>> triangle, int i,int j,int[][] dp){
-        if(i>triangle.size()-1 || j>triangle.get(i).size()-1) return 0;
-        if(dp[i][j]!=-1) return dp[i][j];
-        if(i==triangle.size()-1){
-            return triangle.get(i).get(j);
+    public int helper(List<List<Integer>>  triangle,int x,int y,int[][] dp){
+        if(x>triangle.size()-1 || y>triangle.get(x).size()-1) return 0;
+        if(dp[x][y]!=-1) return dp[x][y];
+        if(x==triangle.size()-1){
+            return triangle.get(x).get(y);
         }
-        int a=helper(triangle,i+1,j,dp);
-        int b=helper(triangle,i+1,j+1,dp);
-        return dp[i][j]=triangle.get(i).get(j)+Math.min(a,b);
+        int b=helper(triangle,x+1,y,dp);
+        int a=helper(triangle,x+1,y+1,dp);
+        return dp[x][y]=triangle.get(x).get(y)+Math.min(a,b);
     }
-    public int minimumTotal(List<List<Integer>> tri) {
-        int l=tri.size();
-        int[][] dp=new int[l][tri.get(l-1).size()];
-        for(int i=0;i<l;i++) Arrays.fill(dp[i],-1);
-       return helper(tri,0,0,dp);
+    public int minimumTotal(List<List<Integer>> triangle) {
+        int l=triangle.size(),l2=triangle.get(l-1).size();
+        int[][] dp=new int[l][l2];
+        for(int i=0;i<l;i++)
+        Arrays.fill(dp[i],-1);
+        return helper(triangle,0,0,dp);
     }
 }
