d97b5712f09fa5a368fa35844f7b5f542b9539d5
Ronit_sharma004 <118843577+ronit044@users.noreply.github.com>
2024-07-25 22:31:30 +0530

    Time: 21 ms (79.13%) | Memory: 56.5 MB (32.66%) - LeetSync

--- 948-sort-an-array/sort-an-array.java
+++ 948-sort-an-array/sort-an-array.java
@@ -1,10 +1,28 @@
 class Solution {
-    public int[] sortArray(int[] nums) {
-        PriorityQueue<Integer> pq=new PriorityQueue<>();
-        for(int i:nums) pq.add(i);
-        int[] res=new int[nums.length];
-        int k=0;
-        while(!pq.isEmpty()) res[k++]=pq.poll();
+    public int[] merge(int[] arr1,int[] arr2){
+        int l1=arr1.length,l2=arr2.length;
+        int c1=0,c2=0,k=0;
+        int[] res=new int[l1+l2];
+        while(c1<l1 && c2<l2){
+            if(arr1[c1]<=arr2[c2])
+            res[k++]=arr1[c1++];
+            else
+            res[k++]=arr2[c2++];
+        }
+        while(c1<l1){
+            res[k++]=arr1[c1++];
+        }
+        while(c2<l2){
+            res[k++]=arr2[c2++];
+        }
         return res;
+    }
+    public int[] mergeSort(int[] nums,int left,int right){
+        if(left==right) return new int[]{nums[left]};
+        int mid=(left+right)/2;
+        return merge(mergeSort(nums,left,mid),mergeSort(nums,mid+1,right));
+    }
+    public int[] sortArray(int[] nums) {
+       return mergeSort(nums,0,nums.length-1);   
     }
 }
