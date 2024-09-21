fda68ab80fb37a10e2215ee0254b91331d663eea
Ronit_sharma004 <118843577+ronit044@users.noreply.github.com>
2024-09-22 01:49:04 +0530

    Time: 2 ms (54.69%) | Memory: 43.4 MB (30.90%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.java
+++ 22-generate-parentheses/generate-parentheses.java
@@ -1,22 +1,11 @@
 class Solution {
-    public boolean validate(String p){
-        Stack<Character> st=new Stack<>();
-        for(char ch:p.toCharArray()){
-            if(ch=='(') st.push(ch);
-            else{
-                if(st.isEmpty()) return false;
-                st.pop();
-            }
-        }
-        return st.isEmpty();
-    }
     List<String> res=new ArrayList<>();
     public void generate(String st, int open, int close, int n)
     {
         if(st.length() > 2 * n ) {
             return;
         }
-        if(st.length() == 2 * n && validate(st)){
+        if(st.length() == 2 * n){
             res.add(st); return;
         }
         if(open < n) {
