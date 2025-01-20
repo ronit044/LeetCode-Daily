fd1c199bbcb8b3062f769967dfad3d4124896084
Ronit Sharma <118843577+ronit044@users.noreply.github.com>
2025-01-20 15:32:40 +0530

    Time: 0 ms (100.00%) | Memory: 40.7 MB (18.08%) - LeetSync

--- 96-unique-binary-search-trees/unique-binary-search-trees.java
+++ 96-unique-binary-search-trees/unique-binary-search-trees.java
@@ -1,14 +1,17 @@
 class Solution {
-    public int helper(int n){
+    public int helper(int n,int []dp){
         if(n==1 || n==0) return 1;
         if(n==2) return 2;
+        if(dp[n]!=-1) return dp[n];
         int count=0;
         for(int i=1;i<=n;i++){
-            count+=helper(i-1)*helper(n-i);
+            count+=helper(i-1,dp)*helper(n-i,dp);
         } 
-        return count;
+        return dp[n]=count;
     }
     public int numTrees(int n) {
-        return helper(n);
+        int dp[]=new int[n+1];
+        Arrays.fill(dp,-1);
+        return helper(n,dp);
     }
 }
