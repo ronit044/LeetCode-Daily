cc908104fcd2467d1ecfa2d5870cb6087cbb6e43
Ronit Sharma <118843577+ronit044@users.noreply.github.com>
2025-01-20 15:33:58 +0530

    Time: 0 ms (100.00%) | Memory: 40.4 MB (47.77%) - LeetSync

--- 96-unique-binary-search-trees/unique-binary-search-trees.java
+++ 96-unique-binary-search-trees/unique-binary-search-trees.java
@@ -1,7 +1,7 @@
 class Solution {
     public int helper(int n,int []dp){
         if(n==1 || n==0) return 1;
-        if(n==2) return 2;
+        // if(n==2) return 2;
         if(dp[n]!=-1) return dp[n];
         int count=0;
         for(int i=1;i<=n;i++){
